eb4ca07479c5b60fd5a2099ebd09f8ce153baada
shtnkgm <nakagami.shota@gmail.com>
2018-12-02 19:32:17 +0900

    Initial Commit

---
+++ BondiBlue/BondiBlue.h
@@ -0,0 +1,19 @@
+//
+//  BondiBlue.h
+//  BondiBlue
+//
+//  Created by Shota Nakagami on 2018/12/02.
+//  Copyright © 2018 Shota Nakagami. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for BondiBlue.
+FOUNDATION_EXPORT double BondiBlueVersionNumber;
+
+//! Project version string for BondiBlue.
+FOUNDATION_EXPORT const unsigned char BondiBlueVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <BondiBlue/PublicHeader.h>
+
+
