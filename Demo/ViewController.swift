1636eac740db47176f6b33181b103f282f79b563
shtnkgm <nakagami.shota@gmail.com>
2018-12-02 19:51:39 +0900

    add demo target

---
+++ Demo/ViewController.swift
@@ -0,0 +1,18 @@
+//
+//  ViewController.swift
+//  Demo
+//
+//  Created by Shota Nakagami on 2018/12/02.
+//  Copyright © 2018 Shota Nakagami. All rights reserved.
+//
+
+import UIKit
+import BondiBlue
+
+class ViewController: UIViewController {
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        view.backgroundColor = .bondiBlue
+    }
+}
+
